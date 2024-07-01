336e2ce959e485529789bb2252a3cd70fc5f94d6
Thomas Watts <57363084+gingertonwatts@users.noreply.github.com>
2024-07-01 10:37:03 -0700

    new python scripts for computing hypergraph metrics

---
+++ mol_db/compute_metrics.py
@@ -0,0 +1,52 @@
+import json
+from compute_graph_metrics import compute_graph_metrics
+
+
+def compute_metrics(H_jw, sample_id, binary):
+    pstring_dict = {}
+
+
+    for ops in H_jw:
+        pstring = str(ops)
+        index1 = pstring.index("[")
+        index2 = pstring.index("]")
+        p_key = pstring[index1:index2 + 1]
+        p_value = complex(pstring[:index1])
+
+        pstring_dict[p_key] = p_value
+
+
+
+    metrics = {}
+    metrics["induced_norm"] = H_jw.induced_norm(order=1)
+
+    # Compute graph based metrics
+    n_qubits, vertex_deg_stats, edge_weight_stats, edge_order_stats = compute_graph_metrics(pstring_dict)
+
+    metrics['total_pstrings'] = len(list(pstring_dict.keys()))
+
+    metrics['max_vertex_degree'] = vertex_deg_stats[0]
+    metrics['avg_vertex_degree'] = vertex_deg_stats[1]
+    metrics['vertex_degree_std_dev'] = vertex_deg_stats[2]
+
+    metrics['max_edge_weight'] = edge_weight_stats[0]
+    metrics['min_edge_weight'] = edge_weight_stats[1]
+    metrics['avg_edge_weight'] = edge_weight_stats[2]
+    metrics['edge_weight_std_dev'] = edge_weight_stats[3]
+
+    metrics['max_edge_order'] = edge_order_stats[0]
+    metrics['min_edge_order'] = edge_order_stats[1]
+    metrics['avg_edge_order'] = edge_order_stats[2]
+    metrics['edge_order_std_dev'] = edge_order_stats[3]
+
+    metrics['n_qubits'] = n_qubits
+
+    if binary == 0:
+        path = "random_H/"
+    else:
+        path = "free_fermion_stats/"
+
+    with open(path + str(sample_id) + "_metrics.json", "w") as write_file:
+        json.dump(metrics, write_file, indent=4, sort_keys=True)
+
+    return metrics
