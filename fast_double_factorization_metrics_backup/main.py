60e6a1f022b4ffd5623d5e6b3a851fcf95bb470a
Thomas Watts <57363084+gingertonwatts@users.noreply.github.com>
2024-10-18 18:58:29 -0700

    new main <= 30 orbs

--- fast_double_factorization_metrics_backup/main.py
+++ fast_double_factorization_metrics_backup/main.py
@@ -3,33 +3,12 @@
 Example usage from an FCIDUMP file
 
 """
-from json_to_metrics_csv import compute_metrics_to_csv
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
+from closedfermion.Transformations.quartic_dirac_transforms import double_factorization_from_quartic
 from faux_ham import *
-from pyscf.tools import fcidump
-import numpy as np
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.mixture import GaussianMixture
-
-from closedfermion.Models.Molecular.QuarticDirac import QuarticDirac
-from closedfermion.Transformations.fermionic_encodings import fermion_to_qubit_transformation
-from closedfermion.Transformations.quartic_dirac_transforms import majorana_operator_from_quartic, double_factorization_from_quartic
 from json_to_metrics_csv import truncate_df_eigenvalues
-
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
-from sklearn.mixture import GaussianMixture
-from scipy.stats import gaussian_kde
-from sklearn.neighbors import KernelDensity
-
-import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.mixture import GaussianMixture
-from sklearn.preprocessing import StandardScaler
-
 
 if __name__ == "__main__":
     filenames = get_chk_filenames("chks/")
@@ -37,7 +16,7 @@ if __name__ == "__main__":
 
     chks=[]
     for chk_file in filenames:
-        if 'VDZ' in chk_file and 'h5' not in chk_file:
+        if 'VDZ' in chk_file:
             print(chk_file)
             chks.append(chk_file)
 
@@ -46,13 +25,11 @@ if __name__ == "__main__":
 
     g_reduced_vectors=[]
     norbs = []
-
+    metrics_list = []
     for fname in tqdm(chks):
-
-
         mol, _, hcore, norb, eri_4d = load_chk(fname)
 
-        if norb <= 20:
+        if norb <= 30:
 
             quartic_fermion = QuarticDirac(eri_4d, hcore, norb)
 
@@ -64,22 +41,22 @@ if __name__ == "__main__":
             g_flattened = gs.reshape(len(eigs), norb * norb)  # Flatten each tensor to a vector of size N*N
 
             # Step 2: Use the lambdas to compute the weighted sum of flattened tensors
-            g_reduced = np.dot(eigs/np.linalg.norm(eigs), g_flattened)  # Weighted sum across the L tensors
+            g_reduced = np.dot(eigs/np.linalg.norm(eigs,ord=1), g_flattened)  # Weighted sum across the L tensors
 
             # Step 3: Store the reduced vector (size N * N) for the i-th sample
             g_reduced_vectors.append(g_reduced)
 
             norbs.append(norb)
 
-    print(max(norbs))
+
     # # Step 1: Find the maximum length of the vectors
     gs_sample = []
     max_length = max(norbs)**2
     for g in g_reduced_vectors:
         g_vec = np.concatenate((g, np.zeros(max_length-len(g))))
-        print(len(g_vec))
         gs_sample.append(g_vec)
 
+    print(norbs)
     gs_sample = np.array(gs_sample)
     #
     # # Assuming g_reduced_vectors is already computed (size M x N*N)
@@ -95,7 +72,7 @@ if __name__ == "__main__":
     gmm.fit(g_reduced_pca)
 
     # Step 3: Sample from the GMM in the reduced space
-    n_samples = 100  # Example: sample 50 new points
+    n_samples = 1000  # Example: sample 50 new points
     g_samples_reduced = gmm.sample(n_samples=n_samples)[0]
 
     # Step 4: Optionally, inverse transform the sampled data back to the original space
@@ -117,4 +94,5 @@ if __name__ == "__main__":
     plt.legend()
     plt.grid(True)
     plt.show()
+
 
