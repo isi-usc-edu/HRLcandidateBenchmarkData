64dcd4fe6aa4626b6853717930c4a5358bf505e9
Thomas Watts <57363084+gingertonwatts@users.noreply.github.com>
2024-10-31 17:17:07 -0700

    Add files via upload

--- fast_double_factorization_metrics/transition_metal_metrics.py
+++ fast_double_factorization_metrics/transition_metal_metrics.py
@@ -5,9 +5,11 @@ import numpy as np
 from pyscf import scf, ao2mo
 from pyscf.lib import chkfile
 
+from closedfermion.Models.Molecular.QuarticDirac import QuarticDirac
 from closedfermion.Transformations.fermionic_encodings import fermion_to_qubit_transformation
 from closedfermion.Transformations.quartic_dirac_transforms import majorana_operator_from_quartic, \
     double_factorization_from_quartic
+from compute_metrics import compute_hypergraph_metrics
 
 
 def get_chk_filenames(path: str, filter_str: str = None):
